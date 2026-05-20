f80fb9049e48d7400a18a40df4cc38f854b61b69
ultimateIX <74211977+ultimateIX@users.noreply.github.com>
2026-05-20 00:00:52 -0600

    Mejora UI login dashboard y configuracion logout

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,12 +3,24 @@ from django.contrib.auth.views import LoginView, LogoutView
 from .views import dashboard
 
 urlpatterns = [
-    path('', LoginView.as_view(
-        template_name='pages/login.html',
-        redirect_authenticated_user=True
-    ), name='login'),
+    path(
+        '',
+        LoginView.as_view(
+            template_name='pages/login.html',
+            redirect_authenticated_user=True
+        ),
+        name='login'
+    ),
 
-    path('dashboard/', dashboard, name='dashboard'),
+    path(
+        'dashboard/',
+        dashboard,
+        name='dashboard'
+    ),
 
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path(
+        'logout/',
+        LogoutView.as_view(next_page='login'),
+        name='logout'
+    ),
 ]
