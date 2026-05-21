6c8f1a1457372580386c27887d43b3861bee82e5
ultimateIX <74211977+ultimateIX@users.noreply.github.com>
2026-05-20 22:50:03 -0600

    Implentacion de htmx para futuro ajax

--- inventario/views.py
+++ inventario/views.py
@@ -10,15 +10,27 @@ def inventario_lista(request):
 
 @login_required
 def equipo_crear(request):
+    es_htmx = request.headers.get('HX-Request')
+
     if request.method == 'POST':
         form = EquipoForm(request.POST)
 
         if form.is_valid():
             form.save()
+
+            if es_htmx:
+                return render(request, 'pages/inventario/formulario.html', {
+                    'form': EquipoForm(),
+                    'es_modal': True,
+                    'guardado': True,
+                })
+
             return redirect('inventario:lista')
     else:
         form = EquipoForm()
 
     return render(request, 'pages/inventario/formulario.html', {
-        'form': form
+        'form': form,
+        'es_modal': bool(es_htmx),
+        'guardado': False,
     })
